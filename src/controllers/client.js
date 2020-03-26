963c42fa7f1e8a720e32ab61e80ee5236ac22309
Juan Carlos <Sulbaranjc@gmail.com>
2020-03-26 03:10:14 -0300

    ok

--- src/controllers/client.js
+++ src/controllers/client.js
@@ -3,19 +3,16 @@ const ClientCtrl = {};
 const Client = require("../models/client");
 
 ClientCtrl.getClient = async (req, res) => {
-  const Client = await Client.find();
-  res.json(users);
+  const messages = await Client.find();
+  res.json(messages);
 };
 
 ClientCtrl.createClient = async (req, res) => {
-  const client  = req.body;
-  const newClient = new Client( client );
+  const client = req.body;
+  const newClient = new Client(client);
   await newClient.save();
   res.json("Client Created");
 };
-
-
-
 
 ClientCtrl.deleteClient = async (req, res) => {
   await Client.findByIdAndDelete(req.params.id);
